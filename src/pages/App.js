18f203157e26492496588df6606b37c3ad02382f
Danny <223181@ids.upchiapas.edu.mx>
2024-03-03 15:13:10 -0600

    Implementacion y conexion con la api server-ws

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,27 +1,73 @@
-import { useNotifications } from "./hooks/useNotifications";
-import { postProduct } from "../src/services/services/postProduct";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
-function App() {
-  const { data, loading } = useNotifications();
-
-  
-  const product = {
-    title: "Nombre del Producto",
-    price: 10.99,
-    description: "Descripción del producto",
-   
+function useNotifications() {
+  // Define la función notify aquí
+  const notify = (message) => {
+    alert(message);
   };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    postProduct(product); 
+  return { notify };
+}
+
+function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [name, setProducto] = useState("");
+  const [price, setPrecio] = useState("");
+
+  const { notify } = useNotifications(); // Utiliza el hook useNotifications para obtener la función notify
+
+  useEffect(() => {
+    const socket = io("http://localhost:5000");
+    socket.on("receiveData", (orden) => {
+      console.log(orden);
+      notify("Pago realizado: " + JSON.stringify(orden));
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [notify]);
+
+  const handleClick = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:15672/api/order", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          price
+        }),
+      });
+      const responseData = await response.json();
+      setData(responseData);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error al agregar producto:", error);
+      setLoading(false);
+    }
   };
 
   return (
     <>
-      {data !== null && loading === false && <h1>{data?.title}</h1>}
+      {data !== null && loading === false && <h1>{data.title}</h1>}
       <div className="card">
+        <input
+          type="text"
+          placeholder="Producto"
+          value={name}
+          onChange={(e) => setProducto(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Precio"
+          value={price}
+          onChange={(e) => setPrecio(e.target.value)}
+        />
         <button onClick={handleClick}>Agregar Producto</button>
         {loading && data === null && "Cargando..."}
       </div>
